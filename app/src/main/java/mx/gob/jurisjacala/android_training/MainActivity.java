9791510a737cee9a02a367882bebf8eb7b84459a
Christian Y. Castillo Covarrubias <cyccov@gmail.com>
2021-08-14 21:12:58 -0500

    --
    cambio de proyecto

--- app/src/main/java/mx/gob/jurisjacala/android_training/MainActivity.java
+++ app/src/main/java/mx/gob/jurisjacala/android_training/MainActivity.java
@@ -1,73 +1,52 @@
 package mx.gob.jurisjacala.android_training;
 
-import android.os.Bundle;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.bottomappbar.BottomAppBar;
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import mx.gob.jurisjacala.android_training.databinding.ActivityMainBinding;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
-    private BottomAppBar bottomAppBar;
-    
-
+    private EditText etNota1, etNota2;
+    private Button btnProcesar;
+    private TextView tvPromedio, tvCondicion;
+    private int nota1,nota2;
+    String condicion;
+    double promedio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        //obtener los valores del layout
+        etNota1 = (EditText) this.findViewById(R.id.editTextNota1);
+        etNota2 = (EditText) findViewById(R.id.editTextNota2);
+        tvPromedio = (TextView)findViewById(R.id.textViewPromedio);
+        tvCondicion = (TextView) findViewById(R.id.textViewCondicion);
+        btnProcesar = (Button)findViewById(R.id.btnProcesar);
 
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+        btnProcesar.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v){
+                //Obtener los valores de los controles
+                nota1 = Integer.parseInt(etNota1.getText().toString());
+                nota2 = Integer.parseInt(etNota2.getText().toString());
+                promedio = nota1+nota2/2.0;
+
+                //determina la condicion del alumno
+                if(promedio <= 9.99){
+                    condicion = "Reprobado";
+                } else if (promedio <= 12.99){
+                    condicion = "Recuperacion";
+                } else {
+                    condicion = "Aprobado";
+                }
+                //imprimir resultados
+                tvPromedio.setText(String.format("%.2f",promedio));
+                tvCondicion.setText(condicion);
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
     }
 }
