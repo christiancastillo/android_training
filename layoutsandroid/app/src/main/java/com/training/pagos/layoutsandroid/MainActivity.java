55d150a85172e0d2c371ea9637b3f2e4e3fdef7b
Christian Y. Castillo Covarrubias <cyccov@gmail.com>
2021-08-22 22:28:24 -0500

    .-
    Agregue el principio de los ejercicios con Fragments ,hace falta revisar que los fragments se muestren

--- layoutsandroid/app/src/main/java/com/training/pagos/layoutsandroid/MainActivity.java
+++ layoutsandroid/app/src/main/java/com/training/pagos/layoutsandroid/MainActivity.java
@@ -3,12 +3,26 @@ package com.training.pagos.layoutsandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonMensaje;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonMensaje = (Button)findViewById(R.id.buttonMensaje);
+
+        buttonMensaje.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Activity Main", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
