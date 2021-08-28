2fcff30b135d7890237c5cb71ba2bb42ba5ed7e9
Christian Y. Castillo Covarrubias <cyccov@gmail.com>
2021-08-28 17:56:54 -0500

    -
    -

--- Fragments_project/app/src/main/java/com/android/training/fragments/MainActivity.java
+++ Fragments_project/app/src/main/java/com/android/training/fragments/MainActivity.java
@@ -1,14 +1,30 @@
 package com.android.training.fragments;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnAlertBox;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnAlertBox = (Button)findViewById(R.id.buttonAlertBox);
+        AlertDialog.Builder dlgAlert = new AlertDialog.Builder(this);
+
+        btnAlertBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dlgAlert.setTitle("Titulo del mensaje de alerta");
+                dlgAlert.setPositiveButton("OK",null);
+                dlgAlert.setMessage("Mensaje de la alerta");
+                dlgAlert.create().show();
+            }
+        });
     }
 }
